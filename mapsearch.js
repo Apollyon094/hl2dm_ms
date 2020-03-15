68309009dad4cb9b601965d5dff963b263ccbc74
Apollyon094 <51381090+Apollyon094@users.noreply.github.com>
2020-03-15 11:39:04 -0700

    Update mapsearch.js

--- mapsearch.js
+++ mapsearch.js
@@ -1,43 +1,32 @@
-const http = require("http");
+const ht = require("http");
 const fs = require("fs");
 const rl = require("readline-sync");
+const sh = require("sha256");
+
+function repeat(func, times) {
+    func();
+    times && --times && repeat(func, times);
+}
 
 const name = rl.question("Enter a map name. \n");
 const fastdls = ["http://fusionheadweb.site.nfoservers.com/maps/", "http://gawp.fastdl.scorpex.org/maps/", "http://outlaw.fastdl.scorpex.org/maps/", "http://clans.playugh.com/66f1190c/hl2dm/maps/"];
-const cheese = 1;
+var cheese = 0;
 
-const file = fs.createWriteStream("./" + name + ".bsp.bz2");
-function request(yeah) {
-	http.get(fastdls[yeah] + name + ".bsp.bz2", function(response) {
+const file = fs.createWriteStream("./" + name + "_" + sh(fastdls[0]) + ".bsp.bz2");
+
+function request(foo, bar) {
+	ht.get(fastdls[foo] + bar +  ".bsp.bz2", function(response) {
 		response.pipe(file);
-		console.log("File found from " + fastdls[cheese] + "!");
+		console.log("File found from " + fastdls[foo] + "!");
 	})
 }
 
-const file2 = fs.createWriteStream("./" + name + "-temp" + ".bsp.bz2");
-function request2(yeah) {
-	http.get(fastdls[yeah] + name + ".bsp.bz2", function(response2) {
-		response.pipe(file2);
-		console.log("File found from " + fastdls[cheese] + "!");
-	})
-}
-
-request(cheese);
-
-const stats = fs.statSync("./" + name + ".bsp.bz2")
-const stats2 = fs.statSync("./" + name + "-temp" + ".bsp.bz2")
-const fileSizeInBytes = stats["size"]
-const fileSizeInBytes2 = stats2["size"]
-
-if(fileSizeInBytes2 >= fileSizeInBytes) {
-	const file3 = fs.createWriteStream("./" + name + ".bsp.bz2");
-	function request3(yeah) {
-		http.get(fastdls[yeah] + name + ".bsp.bz2", function(response2) {
-			response.pipe(file3);
-			console.log("File found from " + fastdls[cheese] + "!");
-		})
-	}
-} else {
+function mycodeisreallyterrible() {
+	const file = fs.createWriteStream("./" + name + "_" + sh(fastdls[cheese]) + ".bsp.bz2");
+	request(cheese, name);
 	cheese++;
-	request3(yeah)
 }
+
+const whatever = fastdls.length - 1
+
+repeat(mycodeisreallyterrible, whatever);
