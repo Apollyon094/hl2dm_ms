3e9e7d13d4d0041dc008f0915bef004ade41dcc2
Apollyon094 <51381090+Apollyon094@users.noreply.github.com>
2020-03-15 11:46:41 -0700

    Update mapsearch.js

--- mapsearch.js
+++ mapsearch.js
@@ -1,7 +1,6 @@
 const ht = require("http");
 const fs = require("fs");
 const rl = require("readline-sync");
-const sh = require("sha256");
 
 function repeat(func, times) {
     func();
@@ -12,7 +11,7 @@ const name = rl.question("Enter a map name. \n");
 const fastdls = ["http://fusionheadweb.site.nfoservers.com/maps/", "http://gawp.fastdl.scorpex.org/maps/", "http://outlaw.fastdl.scorpex.org/maps/", "http://clans.playugh.com/66f1190c/hl2dm/maps/"];
 var cheese = 0;
 
-const file = fs.createWriteStream("./" + name + "_" + sh(fastdls[0]) + ".bsp.bz2");
+const file = fs.createWriteStream("./" + name + "_" + cheese + ".bsp.bz2");
 
 function request(foo, bar) {
 	ht.get(fastdls[foo] + bar +  ".bsp.bz2", function(response) {
@@ -22,11 +21,9 @@ function request(foo, bar) {
 }
 
 function mycodeisreallyterrible() {
-	const file = fs.createWriteStream("./" + name + "_" + sh(fastdls[cheese]) + ".bsp.bz2");
+	const file = fs.createWriteStream("./" + name + "_" + cheese + ".bsp.bz2");
 	request(cheese, name);
 	cheese++;
 }
 
-const whatever = fastdls.length - 1
-
-repeat(mycodeisreallyterrible, whatever);
+repeat(mycodeisreallyterrible, 4);
