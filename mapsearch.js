55d41870b0025da5d75d72720dce8f2a845d69eb
Asher I <51381090+Apollyon094@users.noreply.github.com>
2021-02-05 14:23:11 -0800

    Update mapsearch.js

--- mapsearch.js
+++ mapsearch.js
@@ -1,19 +1,17 @@
-const ht = require("http");
-const fs = require("fs");
-const rl = require("readline-sync");
-async function repeat(func, times) {
-    await func();
-    times && --times && await repeat(func, times);
+const ht = require ("http");
+const fs = require ("fs");
+const rl = require ("readline-sync");
+async function repeat (func, times) {
+    await func ();
+    times && --times && await repeat (func, times);
 }
-
-const name = rl.question("Enter a map name. \n");
+const name = rl.question ("Enter a map name. \n");
 const fastdls = ["http://fusionheadweb.site.nfoservers.com/maps/", "http://gawp.fastdl.scorpex.org/maps/", "http://outlaw.fastdl.scorpex.org/maps/", "http://clans.playugh.com/66f1190c/hl2dm/maps/"];
-const file = fs.createWriteStream("./" + name + ".bsp.bz2");
-function request(foo, bar) {
-	ht.get(fastdls[foo] + bar +  ".bsp.bz2", function(response) {
-		response.pipe(file);
-		console.log("File found from " + fastdls[foo] + "!");
+const file = fs.createWriteStream ("./" + name + ".bsp.bz2");
+function request (foo, bar) {
+	ht.get (fastdls [foo] + bar + ".bsp.bz2", function (response) {
+		response.pipe (file);
+		console.log ("File found from " + fastdls [foo] + "!");
 	})
 }
-
-request(0, name);
+request (0, name);
